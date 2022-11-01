7058d7cc43348b57d73422ad31a8371d5e3630c2
mrbaummath <baum1286@gmail.com>
2022-11-01 14:42:39 -0400

    solved issue with activities not loading prior to JSX mapping by introducing a loading screen

---
+++ src/components/shared/LoadingPage.js
@@ -0,0 +1,12 @@
+import { Spinner } from 'react-bootstrap'
+
+const LoadingScreen = () => (
+    <div style={{textAlign: 'center'}}>
+        <Spinner role="status" animation="border">
+            <span className="visually-hidden">Loading...</span>
+        </Spinner>
+    </div>
+
+)
+
+export default LoadingScreen
