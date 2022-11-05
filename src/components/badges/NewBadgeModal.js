a19f039f968c2e1a3e4f1c79bd35312d227303c7
mrbaummath <baum1286@gmail.com>
2022-11-05 15:27:15 -0400

    new badge modal seems to be functioning properly

--- src/components/badges/NewBadgeModal.js
+++ src/components/badges/NewBadgeModal.js
@@ -7,13 +7,42 @@ import ActivityForm from '../shared/ActivityForm'
 const NewBadgeModal = (props) => {
     const {badgeUpdate} = props
     const [open, setOpen] = useState(false)
+    const [modalMessage, setModalMessage] = useState(null) 
 
     useEffect(() => {
-        if (badgeUpdate && badgeUpdate.change) {
-            setOpen((badgeUpdate.change != 'none'))
+        if (badgeUpdate) {
+            if (badgeUpdate.change) {
+                setOpen((badgeUpdate.change != 'none'))
+            }
+            if (badgeUpdate.change === 'lost') {
+                setModalMessage({
+                    'heading': "Badge Lost",
+                    'description': "Keep at it!",
+                    'content': `You lost your ${badgeUpdate.type} badge but you'll earn it back as soon as you complete enough activities`
+                })
+            } else if (badgeUpdate.change === 'up' && badgeUpdate.level === 'novice') {
+                setModalMessage({
+                    'heading': "A Brand New Badge!",
+                    'description': `You've completed enough ${badgeUpdate.type} activities to earn a new badge.`,
+                    'content': `Congratulations, you are now a ${badgeUpdate.level} in ${badgeUpdate.type}`
+                })
+            } else if (badgeUpdate.change === 'up' && badgeUpdate.level !== 'novice') {
+                setModalMessage({
+                    'heading': "Upgraded!",
+                    'description': `You've completed enough ${badgeUpdate.type} activities to earn a new badge level.`,
+                    'content': `Congratulations, you are now a ${badgeUpdate.level} in ${badgeUpdate.type}`
+                })
+            } else if (badgeUpdate.change === 'down') {
+                setModalMessage({
+                    'heading': "That's OK!",
+                    'description': `It's just a temporary setback `,
+                    'content': `You're back to being a ${badgeUpdate.level} in ${badgeUpdate.type} but everything is a learning process. Keep at it.`
+                })
+            }
         }
     },[badgeUpdate])
 
+   
     return(
         <Modal
             closeIcon
@@ -23,15 +52,16 @@ const NewBadgeModal = (props) => {
             onOpen={() => setOpen(true)}
             open={open}
         >
-            <Modal.Header>Change</Modal.Header>
+            <Modal.Header>{modalMessage && modalMessage.heading}</Modal.Header>
             <Modal.Content>
                 <Modal.Description>
-                    <Header>There was a change in your badge status</Header>
-                    <p>This is the change</p>
+                    <Header>{modalMessage && modalMessage.description}</Header>
+                    {modalMessage && <p>{modalMessage.content}</p>}
                 </Modal.Description>
             </Modal.Content>
-      </Modal>
+        </Modal>
     )
+  
 }
 
 export default NewBadgeModal
